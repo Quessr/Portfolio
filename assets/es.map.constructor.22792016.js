42d6bf0cd64533d5b3bdaf9f6933bec59f2945b3
Quessr <Quessr@users.noreply.github.com>
2022-11-28 13:30:19 +0000

    deploy: cf3ccda7cff1dfff01871be46cd6407ff52c7fc1

---
+++ assets/es.map.constructor.22792016.js
@@ -0,0 +1,2 @@
+var r=Object.defineProperty;var t=(n,o)=>r(n,"name",{value:o,configurable:!0});import{n as e,o as c}from"./iframe.0f21793f.js";var i=e,l=c;i("Map",function(n){return t(function(){return n(this,arguments.length?arguments[0]:void 0)},"Map")},l);
+//# sourceMappingURL=es.map.constructor.22792016.js.map
