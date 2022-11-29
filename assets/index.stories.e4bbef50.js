644ff5a17cb82e75826ce6a11873417c6c96f88c
Quessr <Quessr@users.noreply.github.com>
2022-11-29 16:02:28 +0000

    deploy: 6912962ba68ad877c5b520d932f0ce1dfd25439c

---
+++ assets/index.stories.e4bbef50.js
@@ -0,0 +1,18 @@
+var a=Object.defineProperty;var o=(e,n)=>a(e,"name",{value:n,configurable:!0});import{j as t}from"./jsx-runtime.09355bc0.js";import"./iframe.a76f2b4c.js";const s=o(()=>t("div",{children:t("div",{children:"test"})}),"Header"),r=s,d={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+
+import Header from '.';
+
+export default {
+  /* \u{1F447} The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Molecule / Header',
+  component: Header,
+} as ComponentMeta<typeof Header>;
+
+export const API = (props: typeof Header) => {
+  return <Header {...props} />;
+};
+`,locationsMap:{api:{startLoc:{col:19,line:14},endLoc:{col:1,line:16},startBody:{col:19,line:14},endBody:{col:1,line:16}}}}},title:"Molecule / Header",component:r},l=o(e=>t(r,{...e}),"API"),m=["API"];export{l as API,m as __namedExportsOrder,d as default};
+//# sourceMappingURL=index.stories.e4bbef50.js.map
