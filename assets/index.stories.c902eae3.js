6e834fdb3c9cf471d22eb2654dc5d9c15dc05d62
Quessr <Quessr@users.noreply.github.com>
2022-11-30 09:09:41 +0000

    deploy: 746a22c607c2907c66012aa69b70168df272b93b

---
+++ assets/index.stories.c902eae3.js
@@ -0,0 +1,18 @@
+var n=Object.defineProperty;var t=(o,r)=>n(o,"name",{value:r,configurable:!0});import{j as s}from"./jsx-runtime.f29be1ee.js";import"./iframe.b56c5c4e.js";const c=t(()=>null,"Footer"),e=c,i={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+
+import Footer from '.';
+
+export default {
+  /* \u{1F447} The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Molecule / Footer',
+  component: Footer,
+} as ComponentMeta<typeof Footer>;
+
+export const API = (props: typeof Footer) => {
+  return <Footer {...props} />;
+};
+`,locationsMap:{api:{startLoc:{col:19,line:14},endLoc:{col:1,line:16},startBody:{col:19,line:14},endBody:{col:1,line:16}}}}},title:"Molecule / Footer",component:e},m=t(o=>s(e,{...o}),"API"),u=["API"];export{m as API,u as __namedExportsOrder,i as default};
+//# sourceMappingURL=index.stories.c902eae3.js.map
