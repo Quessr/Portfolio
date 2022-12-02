8dc6c56b3148455114d6f935244bfbd816c9f88d
Quessr <Quessr@users.noreply.github.com>
2022-12-02 16:25:32 +0000

    deploy: 578308441a0a735177247d8a8bd4128dc35a402c

---
+++ assets/index.stories.73fa64ab.js
@@ -0,0 +1,19 @@
+var p=Object.defineProperty;var a=(r,n)=>p(r,"name",{value:n,configurable:!0});import{H as e}from"./styled-components.browser.esm.83929992.js";import{T as m,G as l,F as c}from"./index.1183b402.js";import{H as d}from"./index.df059e4a.js";import{C as s}from"./index.505edaf4.js";import{a as o,j as t}from"./jsx-runtime.f2518f2e.js";import"./index.616addcb.js";import"./clsx.m.256e9345.js";import"./index.03cccca7.js";import"./index.41319f1d.js";import"./index.4e25d7ba.js";import"./index.962537e3.js";import"./iframe.4a9ba084.js";const y=a(()=>o(_,{children:[t(d,{}),o(v,{children:[o(C,{children:[t(g,{children:"Get in Touch"}),t(f,{children:"heosera76@gmail.com"}),t(h,{children:"010 - 7626 - 7410"}),o(T,{children:[t(m,{}),t(l,{})]})]}),t(s,{})]}),t(c,{})]}),"ContactTemplate"),i=y;var _=e("div").withConfig({displayName:"ContactTemplate___StyledDiv",componentId:"sc-bzprok-0"})({display:"grid",rowGap:"7rem"}),v=e("div").withConfig({displayName:"ContactTemplate___StyledDiv2",componentId:"sc-bzprok-1"})({display:"grid",gridTemplateColumns:"repeat(2, minmax(0, 1fr))",paddingLeft:"7rem",paddingRight:"7rem"}),C=e("div").withConfig({displayName:"ContactTemplate___StyledDiv3",componentId:"sc-bzprok-2"})({display:"grid",height:"280px","--tw-text-opacity":"1",color:"rgb(var(--color-secondary) / var(--tw-text-opacity))"}),g=e("h3").withConfig({displayName:"ContactTemplate___StyledH",componentId:"sc-bzprok-3"})({fontSize:"2.25rem",lineHeight:"2.5rem"}),f=e("p").withConfig({displayName:"ContactTemplate___StyledP",componentId:"sc-bzprok-4"})({fontSize:"1.25rem",lineHeight:"1.75rem"}),h=e("p").withConfig({displayName:"ContactTemplate___StyledP2",componentId:"sc-bzprok-5"})({fontSize:"1.25rem",lineHeight:"1.75rem"}),T=e("div").withConfig({displayName:"ContactTemplate___StyledDiv4",componentId:"sc-bzprok-6"})({display:"flex",gap:"0.75rem",fill:"rgb(var(--color-secondary) / 1)"});const A={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
+
+import ContactTemplate from '.';
+
+export default {
+  title: 'Templates / ContactTemplate',
+  component: ContactTemplate,
+} as ComponentMeta<typeof ContactTemplate>;
+
+export const API = (props: typeof ContactTemplate) => {
+  return (
+    <div tw="bg-primary">
+      <ContactTemplate {...props} />
+    </div>
+  );
+};
+`,locationsMap:{api:{startLoc:{col:19,line:11},endLoc:{col:1,line:17},startBody:{col:19,line:11},endBody:{col:1,line:17}}}}},title:"Templates / ContactTemplate",component:i},F=a(r=>t(S,{children:t(i,{...r})}),"API");var S=e("div").withConfig({displayName:"indexstories___StyledDiv",componentId:"sc-mqkuwv-0"})({"--tw-bg-opacity":"1",backgroundColor:"rgb(var(--color-primary) / var(--tw-bg-opacity))"});const L=["API"];export{F as API,L as __namedExportsOrder,A as default};
+//# sourceMappingURL=index.stories.73fa64ab.js.map
