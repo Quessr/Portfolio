de2f0452a71a894e2061e469b5abc627e40a3ae9
Quessr <Quessr@users.noreply.github.com>
2022-12-02 07:16:40 +0000

    deploy: 2b3eb5c671d665ecd47313fff89a1922124270ff

---
+++ assets/index.stories.c302b6bb.js
@@ -0,0 +1,23 @@
+var p=Object.defineProperty;var a=(e,o)=>p(e,"name",{value:o,configurable:!0});import{H as r}from"./styled-components.browser.esm.94c13907.js";import{I as s}from"./index.3e88b4e7.js";import{a as m,j as n}from"./jsx-runtime.bfb412a8.js";import"./clsx.m.256e9345.js";import"./iframe.6300972d.js";const t=a(({name:e,inputTitle:o,...l})=>m(d,{children:[n(u,{htmlFor:e,children:o}),n(s,{name:e,...l})]}),"LabelInput"),i=t;var d=r("div").withConfig({displayName:"LabelInput___StyledDiv",componentId:"sc-1ezrm83-0"})({display:"grid"}),u=r("label").withConfig({displayName:"LabelInput___StyledLabel",componentId:"sc-1ezrm83-1"})({marginBottom:"0.5rem","--tw-text-opacity":"1",color:"rgb(var(--color-secondary) / var(--tw-text-opacity))"});try{t.displayName="LabelInput",t.__docgenInfo={description:"",displayName:"LabelInput",props:{name:{defaultValue:null,description:"",name:"name",required:!0,type:{name:"string"}},inputTitle:{defaultValue:null,description:"",name:"inputTitle",required:!0,type:{name:"string"}},size:{defaultValue:null,description:"",name:"size",required:!1,type:{name:"enum",value:[{value:'"sm"'},{value:'"md"'},{value:'"lg"'}]}},focusOutline:{defaultValue:null,description:"",name:"focusOutline",required:!1,type:{name:"boolean"}}}},typeof STORYBOOK_REACT_CLASSES<"u"&&(STORYBOOK_REACT_CLASSES["src/components/atoms/LabelInput/index.tsx#LabelInput"]={docgenInfo:t.__docgenInfo,name:"LabelInput",path:"src/components/atoms/LabelInput/index.tsx#LabelInput"})}catch{}const L={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
+
+import LabelInput from '.';
+
+export default {
+  /* \u{1F447} The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Atoms / LabelInput',
+  component: LabelInput,
+} as ComponentMeta<typeof LabelInput>;
+
+export const API = (props: typeof LabelInput) => {
+  return (
+    <div tw="bg-primary px-4 py-4">
+      <LabelInput name="Name" inputTitle="Name *" size="sm" {...props} />
+    </div>
+  );
+};
+`,locationsMap:{api:{startLoc:{col:19,line:15},endLoc:{col:1,line:21},startBody:{col:19,line:15},endBody:{col:1,line:21}}}}},title:"Atoms / LabelInput",component:i},v=a(e=>n(c,{children:n(i,{name:"Name",inputTitle:"Name *",size:"sm",...e})}),"API");var c=r("div").withConfig({displayName:"indexstories___StyledDiv",componentId:"sc-1shi5l-0"})({"--tw-bg-opacity":"1",backgroundColor:"rgb(var(--color-primary) / var(--tw-bg-opacity))",paddingLeft:"1rem",paddingRight:"1rem",paddingTop:"1rem",paddingBottom:"1rem"});const S=["API"];export{v as API,S as __namedExportsOrder,L as default};
+//# sourceMappingURL=index.stories.c302b6bb.js.map
