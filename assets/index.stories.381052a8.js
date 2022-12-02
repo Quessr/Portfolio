a05c30200b11f4a227ce3110c3bb43eaebe60d06
Quessr <Quessr@users.noreply.github.com>
2022-12-02 06:40:58 +0000

    deploy: 0f1d96828a093a87593a0e7d0b4ddcd0b06ae4cc

---
+++ assets/index.stories.381052a8.js
@@ -0,0 +1,18 @@
+var r=Object.defineProperty;var o=(t,e)=>r(t,"name",{value:e,configurable:!0});import{j as p}from"./jsx-runtime.2657e4f0.js";import"./iframe.ee299b2c.js";const a=o(()=>null,"LabelInput"),n=a,i={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+
+import LabelInput from '.';
+
+export default {
+  /* \u{1F447} The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Atoms / LabelInput',
+  component: LabelInput,
+} as ComponentMeta<typeof LabelInput>;
+
+export const API = (props: typeof LabelInput) => {
+  return <LabelInput {...props} />;
+};
+`,locationsMap:{api:{startLoc:{col:19,line:14},endLoc:{col:1,line:16},startBody:{col:19,line:14},endBody:{col:1,line:16}}}}},title:"Atoms / LabelInput",component:n},u=o(t=>p(n,{...t}),"API"),m=["API"];export{u as API,m as __namedExportsOrder,i as default};
+//# sourceMappingURL=index.stories.381052a8.js.map
