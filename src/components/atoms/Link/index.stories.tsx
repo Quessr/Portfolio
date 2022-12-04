5c91cb5e8ace5b00a63388cdd1078faa22a0f2cf
seraheo <heosera76@gmail.com>
2022-12-04 21:29:43 +0900

    Link 컴포넌트 구현 및 문서화

--- src/components/atoms/Link/index.stories.tsx
+++ src/components/atoms/Link/index.stories.tsx
@@ -1,16 +1,33 @@
 import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
 
 import Link from '.';
 
 export default {
-  /* 👇 The title prop is optional.
-   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-   * to learn how to generate automatic titles
-   */
   title: 'Atoms / Link',
   component: Link,
 } as ComponentMeta<typeof Link>;
 
 export const API = (props: typeof Link) => {
-  return <Link {...props} />;
+  return (
+    <div>
+      <Link {...props}>Blog</Link>
+    </div>
+  );
+};
+
+export const Sizes = () => {
+  return (
+    <div tw="flex gap-7">
+      <Link href="https://quessr.tistory.com/" size="sm">
+        small
+      </Link>
+      <Link href="https://quessr.tistory.com/" size="md" active>
+        medium
+      </Link>
+      <Link href="https://quessr.tistory.com/" size="lg">
+        large
+      </Link>
+    </div>
+  );
 };
--- src/components/atoms/Link/index.tsx
+++ src/components/atoms/Link/index.tsx
@@ -1,8 +1,39 @@
-export type LinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement>;
+import clsx from 'clsx';
+import tw, { styled } from 'twin.macro';
+
+export interface LinkProps
+  extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  size?: 'sm' | 'md' | 'lg';
+  active?: boolean;
+}
 // Pick<ButtonProps, 'size' | 'variant' | 'ellipse' | 'colorScheme'>;
 
-const Link = () => {
-  return null;
+const Link = ({ className, size, active, children, href }: LinkProps) => {
+  return (
+    <StyledLink
+      href={href}
+      className={clsx(className, `link-${size}`, active && 'link-active')}
+    >
+      {children}
+    </StyledLink>
+  );
 };
 
+const StyledLink = styled.a`
+  //size
+  &.link-sm {
+    ${tw`h-10 text-sm`}
+  }
+  &.link-md {
+    ${tw`h-12 text-base`}
+  }
+  &.link-lg {
+    ${tw`h-14 text-[32px]`}
+  }
+
+  //active
+  &.link-active {
+    ${tw`underline underline-offset-8`}
+  }
+`;
 export default Link;
