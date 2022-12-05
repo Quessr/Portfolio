c5fc107a9505d28d66d1aa7b4ded878fa8a1795f
Quessr <Quessr@users.noreply.github.com>
2022-12-05 02:03:49 +0000

    deploy: 18856ecd48ae83be4c65d063eb3bf1e96607458b

---
+++ assets/index.stories.021bc881.js
@@ -0,0 +1,18 @@
+var n=Object.defineProperty;var t=(o,r)=>n(o,"name",{value:r,configurable:!0});import{F as e}from"./index.41bf4e4a.js";import{j as s}from"./jsx-runtime.1e7b249e.js";import"./styled-components.browser.esm.2fa4782e.js";import"./iframe.3abcf0f7.js";const m={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+
+import Footer from '.';
+
+export default {
+  /* \u{1F447} The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Molecules / Footer',
+  component: Footer,
+} as ComponentMeta<typeof Footer>;
+
+export const API = (props: typeof Footer) => {
+  return <Footer {...props} />;
+};
+`,locationsMap:{api:{startLoc:{col:19,line:14},endLoc:{col:1,line:16},startBody:{col:19,line:14},endBody:{col:1,line:16}}}}},title:"Molecules / Footer",component:e},d=t(o=>s(e,{...o}),"API"),u=["API"];export{d as API,u as __namedExportsOrder,m as default};
+//# sourceMappingURL=index.stories.021bc881.js.map
---
+++ assets/index.stories.bd68047b.js
@@ -0,0 +1,18 @@
+var n=Object.defineProperty;var e=(o,r)=>n(o,"name",{value:r,configurable:!0});import{H as t}from"./index.62d406c1.js";import{j as a}from"./jsx-runtime.1e7b249e.js";import"./styled-components.browser.esm.2fa4782e.js";import"./index.656229c0.js";import"./clsx.m.256e9345.js";import"./iframe.3abcf0f7.js";const u={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+
+import Header from '.';
+
+export default {
+  /* \u{1F447} The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Molecules / Header',
+  component: Header,
+} as ComponentMeta<typeof Header>;
+
+export const API = (props: typeof Header) => {
+  return <Header {...props} />;
+};
+`,locationsMap:{api:{startLoc:{col:19,line:14},endLoc:{col:1,line:16},startBody:{col:19,line:14},endBody:{col:1,line:16}}}}},title:"Molecules / Header",component:t},f=e(o=>a(t,{...o}),"API"),H=["API"];export{f as API,H as __namedExportsOrder,u as default};
+//# sourceMappingURL=index.stories.bd68047b.js.map
