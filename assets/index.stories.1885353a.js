da3f1436660eb1c533e49f88b06c4e1f82f6ee0d
Quessr <Quessr@users.noreply.github.com>
2022-12-05 08:33:03 +0000

    deploy: bccb2d30f179020580e10526f8e5432200fb50fd

---
+++ assets/index.stories.1885353a.js
@@ -0,0 +1,23 @@
+var i=Object.defineProperty;var t=(o,e)=>i(o,"name",{value:e,configurable:!0});import{H as a}from"./styled-components.browser.esm.4cc8f03e.js";import{C as n}from"./index.5b10bb78.js";import{j as r}from"./jsx-runtime.93848c7a.js";import"./index.6a6749dc.js";import"./clsx.m.256e9345.js";import"./index.1edb6601.js";import"./index.2133816c.js";import"./index.a86a169d.js";import"./index.0acfb71d.js";import"./iframe.008d8a9f.js";const b={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
+
+import ContactForm from '.';
+
+export default {
+  /* \u{1F447} The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Organisms / ContactForm',
+  component: ContactForm,
+} as ComponentMeta<typeof ContactForm>;
+
+export const API = (props: typeof ContactForm) => {
+  return (
+    <div tw="bg-primary px-6 py-6">
+      <ContactForm {...props} />
+    </div>
+  );
+};
+`,locationsMap:{api:{startLoc:{col:19,line:15},endLoc:{col:1,line:21},startBody:{col:19,line:15},endBody:{col:1,line:21}}}}},title:"Organisms / ContactForm",component:n},w=t(o=>r(p,{children:r(n,{...o})}),"API");var p=a("div").withConfig({displayName:"indexstories___StyledDiv",componentId:"sc-18s7bwr-0"})({"--tw-bg-opacity":"1",backgroundColor:"rgb(var(--color-primary) / var(--tw-bg-opacity))",paddingLeft:"1.5rem",paddingRight:"1.5rem",paddingTop:"1.5rem",paddingBottom:"1.5rem"});const x=["API"];export{w as API,x as __namedExportsOrder,b as default};
+//# sourceMappingURL=index.stories.1885353a.js.map
---
+++ assets/index.stories.8f8da48d.js
@@ -0,0 +1,119 @@
+var C=Object.defineProperty;var i=(u,s)=>C(u,"name",{value:s,configurable:!0});import{H as t}from"./styled-components.browser.esm.4cc8f03e.js";import{B as c}from"./index.4cc7679f.js";import{F as l}from"./index.55c16272.js";import{C as a}from"./index.4ce5591b.js";import{a as e,j as n}from"./jsx-runtime.93848c7a.js";import"./index.6a6749dc.js";import"./clsx.m.256e9345.js";import"./index.2c91208b.js";import"./index.3df5f160.js";import"./iframe.008d8a9f.js";const d=i(()=>e(r,{children:[n(c,{}),n(m,{children:n(a,{datas:[{thumbnail:"https://blog.kakaocdn.net/dn/c9dzbu/btrSLZ8mBIW/ZV8ZFVknnKXK8EIStSOkd0/img.png",title:"Tailwindcss / Customizing-colors",pubDate:"Sat, 03 Dec 2022 23:50:59 +0900",description:`Tailwindcss customizing-colors \uAE30\uB2A5\uC5D0 \uB300\uD55C \uC815\uB9AC
+
+
+Tailwindcss customizing-colors \uAE30\uB2A5 \uC774\uB780?
+\uC0AC\uC6A9\uD55C \uC774\uC720
+\uC0AC\uC6A9 \uBC29\uBC95
+\uC88B\uC740\uC810
+
+ 
+ 
+ 
+ 
+ 
+Tailwindcss`},{thumbnail:"https://blog.kakaocdn.net/dn/c9dzbu/btrSLZ8mBIW/ZV8ZFVknnKXK8EIStSOkd0/img.png",title:"Tailwindcss / Customizing-colors",pubDate:"Sat, 03 Dec 2022 23:50:59 +0900",description:`Tailwindcss customizing-colors \uAE30\uB2A5\uC5D0 \uB300\uD55C \uC815\uB9AC
+
+
+Tailwindcss customizing-colors \uAE30\uB2A5 \uC774\uB780?
+\uC0AC\uC6A9\uD55C \uC774\uC720
+\uC0AC\uC6A9 \uBC29\uBC95
+\uC88B\uC740\uC810
+
+ 
+ 
+ 
+ 
+ 
+Tailwindcss`},{thumbnail:"https://blog.kakaocdn.net/dn/c9dzbu/btrSLZ8mBIW/ZV8ZFVknnKXK8EIStSOkd0/img.png",title:"Tailwindcss / Customizing-colors",pubDate:"Sat, 03 Dec 2022 23:50:59 +0900",description:`Tailwindcss customizing-colors \uAE30\uB2A5\uC5D0 \uB300\uD55C \uC815\uB9AC
+
+
+Tailwindcss customizing-colors \uAE30\uB2A5 \uC774\uB780?
+\uC0AC\uC6A9\uD55C \uC774\uC720
+\uC0AC\uC6A9 \uBC29\uBC95
+\uC88B\uC740\uC810
+
+ 
+ 
+ 
+ 
+ 
+Tailwindcss`},{thumbnail:"https://blog.kakaocdn.net/dn/c9dzbu/btrSLZ8mBIW/ZV8ZFVknnKXK8EIStSOkd0/img.png",title:"Tailwindcss / Customizing-colors",pubDate:"Sat, 03 Dec 2022 23:50:59 +0900",description:`Tailwindcss customizing-colors \uAE30\uB2A5\uC5D0 \uB300\uD55C \uC815\uB9AC
+
+
+Tailwindcss customizing-colors \uAE30\uB2A5 \uC774\uB780?
+\uC0AC\uC6A9\uD55C \uC774\uC720
+\uC0AC\uC6A9 \uBC29\uBC95
+\uC88B\uC740\uC810
+
+ 
+ 
+ 
+ 
+ 
+Tailwindcss`},{thumbnail:"https://blog.kakaocdn.net/dn/c9dzbu/btrSLZ8mBIW/ZV8ZFVknnKXK8EIStSOkd0/img.png",title:"Tailwindcss / Customizing-colors",pubDate:"Sat, 03 Dec 2022 23:50:59 +0900",description:`Tailwindcss customizing-colors \uAE30\uB2A5\uC5D0 \uB300\uD55C \uC815\uB9AC
+
+
+Tailwindcss customizing-colors \uAE30\uB2A5 \uC774\uB780?
+\uC0AC\uC6A9\uD55C \uC774\uC720
+\uC0AC\uC6A9 \uBC29\uBC95
+\uC88B\uC740\uC810
+
+ 
+ 
+ 
+ 
+ 
+Tailwindcss`},{thumbnail:"https://blog.kakaocdn.net/dn/c9dzbu/btrSLZ8mBIW/ZV8ZFVknnKXK8EIStSOkd0/img.png",title:"Tailwindcss / Customizing-colors",pubDate:"Sat, 03 Dec 2022 23:50:59 +0900",description:`Tailwindcss customizing-colors \uAE30\uB2A5\uC5D0 \uB300\uD55C \uC815\uB9AC
+
+
+Tailwindcss customizing-colors \uAE30\uB2A5 \uC774\uB780?
+\uC0AC\uC6A9\uD55C \uC774\uC720
+\uC0AC\uC6A9 \uBC29\uBC95
+\uC88B\uC740\uC810
+
+ 
+ 
+ 
+ 
+ 
+Tailwindcss`},{thumbnail:"https://blog.kakaocdn.net/dn/c9dzbu/btrSLZ8mBIW/ZV8ZFVknnKXK8EIStSOkd0/img.png",title:"Tailwindcss / Customizing-colors",pubDate:"Sat, 03 Dec 2022 23:50:59 +0900",description:`Tailwindcss customizing-colors \uAE30\uB2A5\uC5D0 \uB300\uD55C \uC815\uB9AC
+
+
+Tailwindcss customizing-colors \uAE30\uB2A5 \uC774\uB780?
+\uC0AC\uC6A9\uD55C \uC774\uC720
+\uC0AC\uC6A9 \uBC29\uBC95
+\uC88B\uC740\uC810
+
+ 
+ 
+ 
+ 
+ 
+Tailwindcss`},{thumbnail:"https://blog.kakaocdn.net/dn/c9dzbu/btrSLZ8mBIW/ZV8ZFVknnKXK8EIStSOkd0/img.png",title:"Tailwindcss / Customizing-colors",pubDate:"Sat, 03 Dec 2022 23:50:59 +0900",description:`Tailwindcss customizing-colors \uAE30\uB2A5\uC5D0 \uB300\uD55C \uC815\uB9AC
+
+
+Tailwindcss customizing-colors \uAE30\uB2A5 \uC774\uB780?
+\uC0AC\uC6A9\uD55C \uC774\uC720
+\uC0AC\uC6A9 \uBC29\uBC95
+\uC88B\uC740\uC810
+
+ 
+ 
+ 
+ 
+ 
+Tailwindcss`}]})}),n(l,{})]}),"BlogTemplate"),o=d;var r=t("div").withConfig({displayName:"BlogTemplate___StyledDiv",componentId:"sc-18fsfqh-0"})({display:"grid",rowGap:"7rem","--tw-bg-opacity":"1",backgroundColor:"rgb(var(--color-secondary) / var(--tw-bg-opacity))"}),m=t("div").withConfig({displayName:"BlogTemplate___StyledDiv2",componentId:"sc-18fsfqh-1"})({paddingLeft:"7rem",paddingRight:"7rem"});const E={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
+
+import BlogTemplate from '.';
+
+export default {
+  title: 'Templates / BlogTemplate',
+  component: BlogTemplate,
+} as ComponentMeta<typeof BlogTemplate>;
+
+export const API = (props: typeof BlogTemplate) => {
+  return <BlogTemplate {...props} />;
+};
+`,locationsMap:{api:{startLoc:{col:19,line:11},endLoc:{col:1,line:13},startBody:{col:19,line:11},endBody:{col:1,line:13}}}}},title:"Templates / BlogTemplate",component:o},Z=i(u=>n(o,{...u}),"API"),h=["API"];export{Z as API,h as __namedExportsOrder,E as default};
+//# sourceMappingURL=index.stories.8f8da48d.js.map
