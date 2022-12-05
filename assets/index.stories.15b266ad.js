5326530f3fa1f3fdbf1dc2aa94e3cce151a63738
Quessr <Quessr@users.noreply.github.com>
2022-12-05 08:10:28 +0000

    deploy: b3caad36944ca5f82604102145a4befd313dac86

---
+++ assets/index.stories.15b266ad.js
@@ -0,0 +1,35 @@
+var s=Object.defineProperty;var o=(e,i)=>s(e,"name",{value:i,configurable:!0});import{H as r}from"./styled-components.browser.esm.e6ac3aa9.js";import{L as n}from"./index.129790ee.js";import{j as t,a as l}from"./jsx-runtime.fd59bee2.js";import"./clsx.m.256e9345.js";import"./iframe.058a24d1.js";const f={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
+
+import Link from '.';
+
+export default {
+  title: 'Atoms / Link',
+  component: Link,
+} as ComponentMeta<typeof Link>;
+
+export const API = (props: typeof Link) => {
+  return (
+    <div>
+      <Link {...props}>Blog</Link>
+    </div>
+  );
+};
+
+export const Sizes = () => {
+  return (
+    <div tw="flex gap-7">
+      <Link href="https://quessr.tistory.com/" size="sm">
+        small
+      </Link>
+      <Link href="https://quessr.tistory.com/" size="md" active>
+        medium
+      </Link>
+      <Link href="https://quessr.tistory.com/" size="lg">
+        large
+      </Link>
+    </div>
+  );
+};
+`,locationsMap:{api:{startLoc:{col:19,line:11},endLoc:{col:1,line:17},startBody:{col:19,line:11},endBody:{col:1,line:17}},sizes:{startLoc:{col:21,line:19},endLoc:{col:1,line:33},startBody:{col:21,line:19},endBody:{col:1,line:33}}}}},title:"Atoms / Link",component:n},y=o(e=>t("div",{children:t(n,{...e,children:"Blog"})}),"API"),u=o(()=>l(c,{children:[t(n,{href:"https://quessr.tistory.com/",size:"sm",children:"small"}),t(n,{href:"https://quessr.tistory.com/",size:"md",active:!0,children:"medium"}),t(n,{href:"https://quessr.tistory.com/",size:"lg",children:"large"})]}),"Sizes");var c=r("div").withConfig({displayName:"indexstories___StyledDiv",componentId:"sc-1tdvo08-0"})({display:"flex",gap:"1.75rem"});const k=["API","Sizes"];export{y as API,u as Sizes,k as __namedExportsOrder,f as default};
+//# sourceMappingURL=index.stories.15b266ad.js.map
---
+++ assets/index.stories.5ddef7de.js
@@ -0,0 +1,29 @@
+var n=Object.defineProperty;var t=(e,a)=>n(e,"name",{value:a,configurable:!0});import{H as i}from"./styled-components.browser.esm.e6ac3aa9.js";import{L as r}from"./index.41bdbc7b.js";import{j as o}from"./jsx-runtime.fd59bee2.js";import"./index.8932e9cd.js";import"./clsx.m.256e9345.js";import"./iframe.058a24d1.js";const u={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
+
+import LabelTextarea from '.';
+
+export default {
+  /* \u{1F447} The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Molecules / LabelTextarea',
+  component: LabelTextarea,
+} as ComponentMeta<typeof LabelTextarea>;
+
+export const API = (props: typeof LabelTextarea) => {
+  return (
+    <div tw="bg-primary px-4 py-4">
+      <LabelTextarea
+        name="Name"
+        textareaTitle="Name *"
+        size="md"
+        focusOutline
+        {...props}
+      />
+    </div>
+  );
+};
+`,locationsMap:{api:{startLoc:{col:19,line:15},endLoc:{col:1,line:27},startBody:{col:19,line:15},endBody:{col:1,line:27}}}}},title:"Molecules / LabelTextarea",component:r},y=t(e=>o(p,{children:o(r,{name:"Name",textareaTitle:"Name *",size:"md",focusOutline:!0,...e})}),"API");var p=i("div").withConfig({displayName:"indexstories___StyledDiv",componentId:"sc-g2mj2d-0"})({"--tw-bg-opacity":"1",backgroundColor:"rgb(var(--color-primary) / var(--tw-bg-opacity))",paddingLeft:"1rem",paddingRight:"1rem",paddingTop:"1rem",paddingBottom:"1rem"});const b=["API"];export{y as API,b as __namedExportsOrder,u as default};
+//# sourceMappingURL=index.stories.5ddef7de.js.map
