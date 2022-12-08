039636899177cdf7651e2ac49e850c1a2917dd53
Quessr <Quessr@users.noreply.github.com>
2022-12-08 13:06:17 +0000

    deploy: 2683a89cfa79976878878505f9b479eddf784eae

---
+++ assets/index.stories.e00d3279.js
@@ -0,0 +1,207 @@
+var o=Object.defineProperty;var t=(n,u)=>o(n,"name",{value:u,configurable:!0});import{B as e}from"./index.9e0c078e.js";import{j as i}from"./jsx-runtime.1c36555f.js";import"./styled-components.browser.esm.f40c7e2e.js";import"./index.b43791d5.js";import"./index.be18cee1.js";import"./clsx.m.256e9345.js";import"./index.62f40952.js";import"./index.78eeda3f.js";import"./index.9a27879f.js";import"./index.a4ebaf77.js";import"./iframe.7af151e9.js";const T={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
+
+import BlogTemplate, { BlogTemplateProps } from '.';
+
+export default {
+  title: 'Templates / BlogTemplate',
+  component: BlogTemplate,
+  args: {
+    items: [
+      {
+        title: 'Utility Types',
+        pubDate: '2022-12-03 04:58:03',
+        link: 'https://quessr.tistory.com/2',
+        thumbnail:
+          'https://blog.kakaocdn.net/dn/W4LBv/btrSHNIAFci/kd8VK92Vhnut9F4lSJvjT1/img.png',
+        description:
+          "\\n\\nTypeScript Utily types 4\uAC00\uC9C0 \uC815\uB9AC\\n\\n\\nPartial<Type>\\nRequired<Type>\\nPick<Type, Keys>\\nOmit<Type, Keys>\\n\\n\xA0\\n\xA0\\n\xA0\\n\xA0\\n\xA0\\n\xA0\\nOmit <Type, Keys>\\n\\n\\n\\nexport interface ButtonProps\\n  extends React.ButtonHTMLAttributes<HTMLButtonElement> {\\n  size?: 'sm' | 'md' | 'lg';\\n  variant?: 'fill' | 'outline' | 'ghost';\\n  ellipse?: boolean;\\n  leftIcon?: ReactNode;\\n  rightIcon?: ReactNode;\\n  colorScheme?: 'white' | 'point';\\n}\\n\\n\\nexport interface IconButtonProps\\n  extends Omit<ButtonProps, 'leftIcon' | 'rightIcon' | 'children'> {\\n  icon?: ReactNode;\\n}\\n\uB9CC\uB4E4\uC5B4\uB454 Button \uCEF4\uD3EC\uB10C\uD2B8\uC5D0\uC11C key\uB97C \uAC00\uC838\uC640 \uC544\uC774\uCF58\uB9CC \uB4E4\uC5B4\uAC00\uB294 IconButton \uCEF4\uD3EC\uB10C\uD2B8\uB97C \uB9CC\uB4E4\uAE30 \uC704\uD574 \uC0AC\uC6A9,\uAC00\uC838\uC624\uC9C0 \uC54A\uC73C\uB824\uB294 key\uC18D\uC131\uC740 Omit\uC758 \uB450\uBC88\uC9F8 \uC778\uC790\uC5D0 '|' \uD45C\uC2DC\uB85C \uAD6C\uBD84\uD558\uC5EC \uC81C\uAC70 \uD560 \uC218 \uC788\uB2E4.\\n\\n\uCC38\uC870: https://www.typescriptlang.org/docs/handbook/utility-types.html#omittype-keys\\n",
+      },
+      {
+        title: 'Utility Types',
+        pubDate: '2022-12-03 04:58:03',
+        link: 'https://quessr.tistory.com/2',
+        thumbnail:
+          'https://blog.kakaocdn.net/dn/W4LBv/btrSHNIAFci/kd8VK92Vhnut9F4lSJvjT1/img.png',
+        description:
+          "\\n\\nTypeScript Utily types 4\uAC00\uC9C0 \uC815\uB9AC\\n\\n\\nPartial<Type>\\nRequired<Type>\\nPick<Type, Keys>\\nOmit<Type, Keys>\\n\\n\xA0\\n\xA0\\n\xA0\\n\xA0\\n\xA0\\n\xA0\\nOmit <Type, Keys>\\n\\n\\n\\nexport interface ButtonProps\\n  extends React.ButtonHTMLAttributes<HTMLButtonElement> {\\n  size?: 'sm' | 'md' | 'lg';\\n  variant?: 'fill' | 'outline' | 'ghost';\\n  ellipse?: boolean;\\n  leftIcon?: ReactNode;\\n  rightIcon?: ReactNode;\\n  colorScheme?: 'white' | 'point';\\n}\\n\\n\\nexport interface IconButtonProps\\n  extends Omit<ButtonProps, 'leftIcon' | 'rightIcon' | 'children'> {\\n  icon?: ReactNode;\\n}\\n\uB9CC\uB4E4\uC5B4\uB454 Button \uCEF4\uD3EC\uB10C\uD2B8\uC5D0\uC11C key\uB97C \uAC00\uC838\uC640 \uC544\uC774\uCF58\uB9CC \uB4E4\uC5B4\uAC00\uB294 IconButton \uCEF4\uD3EC\uB10C\uD2B8\uB97C \uB9CC\uB4E4\uAE30 \uC704\uD574 \uC0AC\uC6A9,\uAC00\uC838\uC624\uC9C0 \uC54A\uC73C\uB824\uB294 key\uC18D\uC131\uC740 Omit\uC758 \uB450\uBC88\uC9F8 \uC778\uC790\uC5D0 '|' \uD45C\uC2DC\uB85C \uAD6C\uBD84\uD558\uC5EC \uC81C\uAC70 \uD560 \uC218 \uC788\uB2E4.\\n\\n\uCC38\uC870: https://www.typescriptlang.org/docs/handbook/utility-types.html#omittype-keys\\n",
+      },
+      {
+        title: 'Utility Types',
+        pubDate: '2022-12-03 04:58:03',
+        link: 'https://quessr.tistory.com/2',
+        thumbnail:
+          'https://blog.kakaocdn.net/dn/W4LBv/btrSHNIAFci/kd8VK92Vhnut9F4lSJvjT1/img.png',
+        description:
+          "\\n\\nTypeScript Utily types 4\uAC00\uC9C0 \uC815\uB9AC\\n\\n\\nPartial<Type>\\nRequired<Type>\\nPick<Type, Keys>\\nOmit<Type, Keys>\\n\\n\xA0\\n\xA0\\n\xA0\\n\xA0\\n\xA0\\n\xA0\\nOmit <Type, Keys>\\n\\n\\n\\nexport interface ButtonProps\\n  extends React.ButtonHTMLAttributes<HTMLButtonElement> {\\n  size?: 'sm' | 'md' | 'lg';\\n  variant?: 'fill' | 'outline' | 'ghost';\\n  ellipse?: boolean;\\n  leftIcon?: ReactNode;\\n  rightIcon?: ReactNode;\\n  colorScheme?: 'white' | 'point';\\n}\\n\\n\\nexport interface IconButtonProps\\n  extends Omit<ButtonProps, 'leftIcon' | 'rightIcon' | 'children'> {\\n  icon?: ReactNode;\\n}\\n\uB9CC\uB4E4\uC5B4\uB454 Button \uCEF4\uD3EC\uB10C\uD2B8\uC5D0\uC11C key\uB97C \uAC00\uC838\uC640 \uC544\uC774\uCF58\uB9CC \uB4E4\uC5B4\uAC00\uB294 IconButton \uCEF4\uD3EC\uB10C\uD2B8\uB97C \uB9CC\uB4E4\uAE30 \uC704\uD574 \uC0AC\uC6A9,\uAC00\uC838\uC624\uC9C0 \uC54A\uC73C\uB824\uB294 key\uC18D\uC131\uC740 Omit\uC758 \uB450\uBC88\uC9F8 \uC778\uC790\uC5D0 '|' \uD45C\uC2DC\uB85C \uAD6C\uBD84\uD558\uC5EC \uC81C\uAC70 \uD560 \uC218 \uC788\uB2E4.\\n\\n\uCC38\uC870: https://www.typescriptlang.org/docs/handbook/utility-types.html#omittype-keys\\n",
+      },
+      {
+        title: 'Utility Types',
+        pubDate: '2022-12-03 04:58:03',
+        link: 'https://quessr.tistory.com/2',
+        thumbnail:
+          'https://blog.kakaocdn.net/dn/W4LBv/btrSHNIAFci/kd8VK92Vhnut9F4lSJvjT1/img.png',
+        description:
+          "\\n\\nTypeScript Utily types 4\uAC00\uC9C0 \uC815\uB9AC\\n\\n\\nPartial<Type>\\nRequired<Type>\\nPick<Type, Keys>\\nOmit<Type, Keys>\\n\\n\xA0\\n\xA0\\n\xA0\\n\xA0\\n\xA0\\n\xA0\\nOmit <Type, Keys>\\n\\n\\n\\nexport interface ButtonProps\\n  extends React.ButtonHTMLAttributes<HTMLButtonElement> {\\n  size?: 'sm' | 'md' | 'lg';\\n  variant?: 'fill' | 'outline' | 'ghost';\\n  ellipse?: boolean;\\n  leftIcon?: ReactNode;\\n  rightIcon?: ReactNode;\\n  colorScheme?: 'white' | 'point';\\n}\\n\\n\\nexport interface IconButtonProps\\n  extends Omit<ButtonProps, 'leftIcon' | 'rightIcon' | 'children'> {\\n  icon?: ReactNode;\\n}\\n\uB9CC\uB4E4\uC5B4\uB454 Button \uCEF4\uD3EC\uB10C\uD2B8\uC5D0\uC11C key\uB97C \uAC00\uC838\uC640 \uC544\uC774\uCF58\uB9CC \uB4E4\uC5B4\uAC00\uB294 IconButton \uCEF4\uD3EC\uB10C\uD2B8\uB97C \uB9CC\uB4E4\uAE30 \uC704\uD574 \uC0AC\uC6A9,\uAC00\uC838\uC624\uC9C0 \uC54A\uC73C\uB824\uB294 key\uC18D\uC131\uC740 Omit\uC758 \uB450\uBC88\uC9F8 \uC778\uC790\uC5D0 '|' \uD45C\uC2DC\uB85C \uAD6C\uBD84\uD558\uC5EC \uC81C\uAC70 \uD560 \uC218 \uC788\uB2E4.\\n\\n\uCC38\uC870: https://www.typescriptlang.org/docs/handbook/utility-types.html#omittype-keys\\n",
+      },
+    ],
+  },
+} as ComponentMeta<typeof BlogTemplate>;
+
+export const API = (props: BlogTemplateProps) => {
+  return <BlogTemplate {...props} />;
+};
+`,locationsMap:{api:{startLoc:{col:19,line:51},endLoc:{col:1,line:53},startBody:{col:19,line:51},endBody:{col:1,line:53}}}}},title:"Templates / BlogTemplate",component:e,args:{items:[{title:"Utility Types",pubDate:"2022-12-03 04:58:03",link:"https://quessr.tistory.com/2",thumbnail:"https://blog.kakaocdn.net/dn/W4LBv/btrSHNIAFci/kd8VK92Vhnut9F4lSJvjT1/img.png",description:`
+
+TypeScript Utily types 4\uAC00\uC9C0 \uC815\uB9AC
+
+
+Partial<Type>
+Required<Type>
+Pick<Type, Keys>
+Omit<Type, Keys>
+
+\xA0
+\xA0
+\xA0
+\xA0
+\xA0
+\xA0
+Omit <Type, Keys>
+
+
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: 'sm' | 'md' | 'lg';
+  variant?: 'fill' | 'outline' | 'ghost';
+  ellipse?: boolean;
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
+  colorScheme?: 'white' | 'point';
+}
+
+
+export interface IconButtonProps
+  extends Omit<ButtonProps, 'leftIcon' | 'rightIcon' | 'children'> {
+  icon?: ReactNode;
+}
+\uB9CC\uB4E4\uC5B4\uB454 Button \uCEF4\uD3EC\uB10C\uD2B8\uC5D0\uC11C key\uB97C \uAC00\uC838\uC640 \uC544\uC774\uCF58\uB9CC \uB4E4\uC5B4\uAC00\uB294 IconButton \uCEF4\uD3EC\uB10C\uD2B8\uB97C \uB9CC\uB4E4\uAE30 \uC704\uD574 \uC0AC\uC6A9,\uAC00\uC838\uC624\uC9C0 \uC54A\uC73C\uB824\uB294 key\uC18D\uC131\uC740 Omit\uC758 \uB450\uBC88\uC9F8 \uC778\uC790\uC5D0 '|' \uD45C\uC2DC\uB85C \uAD6C\uBD84\uD558\uC5EC \uC81C\uAC70 \uD560 \uC218 \uC788\uB2E4.
+
+\uCC38\uC870: https://www.typescriptlang.org/docs/handbook/utility-types.html#omittype-keys
+`},{title:"Utility Types",pubDate:"2022-12-03 04:58:03",link:"https://quessr.tistory.com/2",thumbnail:"https://blog.kakaocdn.net/dn/W4LBv/btrSHNIAFci/kd8VK92Vhnut9F4lSJvjT1/img.png",description:`
+
+TypeScript Utily types 4\uAC00\uC9C0 \uC815\uB9AC
+
+
+Partial<Type>
+Required<Type>
+Pick<Type, Keys>
+Omit<Type, Keys>
+
+\xA0
+\xA0
+\xA0
+\xA0
+\xA0
+\xA0
+Omit <Type, Keys>
+
+
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: 'sm' | 'md' | 'lg';
+  variant?: 'fill' | 'outline' | 'ghost';
+  ellipse?: boolean;
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
+  colorScheme?: 'white' | 'point';
+}
+
+
+export interface IconButtonProps
+  extends Omit<ButtonProps, 'leftIcon' | 'rightIcon' | 'children'> {
+  icon?: ReactNode;
+}
+\uB9CC\uB4E4\uC5B4\uB454 Button \uCEF4\uD3EC\uB10C\uD2B8\uC5D0\uC11C key\uB97C \uAC00\uC838\uC640 \uC544\uC774\uCF58\uB9CC \uB4E4\uC5B4\uAC00\uB294 IconButton \uCEF4\uD3EC\uB10C\uD2B8\uB97C \uB9CC\uB4E4\uAE30 \uC704\uD574 \uC0AC\uC6A9,\uAC00\uC838\uC624\uC9C0 \uC54A\uC73C\uB824\uB294 key\uC18D\uC131\uC740 Omit\uC758 \uB450\uBC88\uC9F8 \uC778\uC790\uC5D0 '|' \uD45C\uC2DC\uB85C \uAD6C\uBD84\uD558\uC5EC \uC81C\uAC70 \uD560 \uC218 \uC788\uB2E4.
+
+\uCC38\uC870: https://www.typescriptlang.org/docs/handbook/utility-types.html#omittype-keys
+`},{title:"Utility Types",pubDate:"2022-12-03 04:58:03",link:"https://quessr.tistory.com/2",thumbnail:"https://blog.kakaocdn.net/dn/W4LBv/btrSHNIAFci/kd8VK92Vhnut9F4lSJvjT1/img.png",description:`
+
+TypeScript Utily types 4\uAC00\uC9C0 \uC815\uB9AC
+
+
+Partial<Type>
+Required<Type>
+Pick<Type, Keys>
+Omit<Type, Keys>
+
+\xA0
+\xA0
+\xA0
+\xA0
+\xA0
+\xA0
+Omit <Type, Keys>
+
+
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: 'sm' | 'md' | 'lg';
+  variant?: 'fill' | 'outline' | 'ghost';
+  ellipse?: boolean;
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
+  colorScheme?: 'white' | 'point';
+}
+
+
+export interface IconButtonProps
+  extends Omit<ButtonProps, 'leftIcon' | 'rightIcon' | 'children'> {
+  icon?: ReactNode;
+}
+\uB9CC\uB4E4\uC5B4\uB454 Button \uCEF4\uD3EC\uB10C\uD2B8\uC5D0\uC11C key\uB97C \uAC00\uC838\uC640 \uC544\uC774\uCF58\uB9CC \uB4E4\uC5B4\uAC00\uB294 IconButton \uCEF4\uD3EC\uB10C\uD2B8\uB97C \uB9CC\uB4E4\uAE30 \uC704\uD574 \uC0AC\uC6A9,\uAC00\uC838\uC624\uC9C0 \uC54A\uC73C\uB824\uB294 key\uC18D\uC131\uC740 Omit\uC758 \uB450\uBC88\uC9F8 \uC778\uC790\uC5D0 '|' \uD45C\uC2DC\uB85C \uAD6C\uBD84\uD558\uC5EC \uC81C\uAC70 \uD560 \uC218 \uC788\uB2E4.
+
+\uCC38\uC870: https://www.typescriptlang.org/docs/handbook/utility-types.html#omittype-keys
+`},{title:"Utility Types",pubDate:"2022-12-03 04:58:03",link:"https://quessr.tistory.com/2",thumbnail:"https://blog.kakaocdn.net/dn/W4LBv/btrSHNIAFci/kd8VK92Vhnut9F4lSJvjT1/img.png",description:`
+
+TypeScript Utily types 4\uAC00\uC9C0 \uC815\uB9AC
+
+
+Partial<Type>
+Required<Type>
+Pick<Type, Keys>
+Omit<Type, Keys>
+
+\xA0
+\xA0
+\xA0
+\xA0
+\xA0
+\xA0
+Omit <Type, Keys>
+
+
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: 'sm' | 'md' | 'lg';
+  variant?: 'fill' | 'outline' | 'ghost';
+  ellipse?: boolean;
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
+  colorScheme?: 'white' | 'point';
+}
+
+
+export interface IconButtonProps
+  extends Omit<ButtonProps, 'leftIcon' | 'rightIcon' | 'children'> {
+  icon?: ReactNode;
+}
+\uB9CC\uB4E4\uC5B4\uB454 Button \uCEF4\uD3EC\uB10C\uD2B8\uC5D0\uC11C key\uB97C \uAC00\uC838\uC640 \uC544\uC774\uCF58\uB9CC \uB4E4\uC5B4\uAC00\uB294 IconButton \uCEF4\uD3EC\uB10C\uD2B8\uB97C \uB9CC\uB4E4\uAE30 \uC704\uD574 \uC0AC\uC6A9,\uAC00\uC838\uC624\uC9C0 \uC54A\uC73C\uB824\uB294 key\uC18D\uC131\uC740 Omit\uC758 \uB450\uBC88\uC9F8 \uC778\uC790\uC5D0 '|' \uD45C\uC2DC\uB85C \uAD6C\uBD84\uD558\uC5EC \uC81C\uAC70 \uD560 \uC218 \uC788\uB2E4.
+
+\uCC38\uC870: https://www.typescriptlang.org/docs/handbook/utility-types.html#omittype-keys
+`}]}},A=t(n=>i(e,{...n}),"API"),g=["API"];export{A as API,g as __namedExportsOrder,T as default};
+//# sourceMappingURL=index.stories.e00d3279.js.map
---
+++ assets/index.stories.e3736b2d.js
@@ -0,0 +1,19 @@
+var n=Object.defineProperty;var e=(o,r)=>n(o,"name",{value:r,configurable:!0});import{H as t}from"./index.949cdb42.js";import{j as a}from"./jsx-runtime.1c36555f.js";import"./styled-components.browser.esm.f40c7e2e.js";import"./index.be18cee1.js";import"./clsx.m.256e9345.js";import"./index.62f40952.js";import"./iframe.7af151e9.js";const f={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
+
+import Header from '.';
+
+export default {
+  /* \u{1F447} The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Molecules / Header',
+  component: Header,
+} as ComponentMeta<typeof Header>;
+
+export const API = (props: typeof Header) => {
+  return <Header {...props} />;
+};
+`,locationsMap:{api:{startLoc:{col:19,line:15},endLoc:{col:1,line:17},startBody:{col:19,line:15},endBody:{col:1,line:17}}}}},title:"Molecules / Header",component:t},H=e(o=>a(t,{...o}),"API"),y=["API"];export{H as API,y as __namedExportsOrder,f as default};
+//# sourceMappingURL=index.stories.e3736b2d.js.map
