f73c962363e4370ddc51897b4a0656a43d6953c6
Quessr <Quessr@users.noreply.github.com>
2022-12-09 08:28:02 +0000

    deploy: c4ae4c95d1c977a47c88c9e85061b8442ae1c5e7

---
+++ assets/index.stories.2f0427d6.js
@@ -0,0 +1,19 @@
+var p=Object.defineProperty;var o=(t,n)=>p(t,"name",{value:n,configurable:!0});import{H as a}from"./styled-components.browser.esm.c6a67696.js";import{C as r}from"./index.e597a907.js";import{j as e}from"./jsx-runtime.202beed9.js";import"./index.df774490.js";import"./index.b543ee28.js";import"./index.40d1ca40.js";import"./clsx.m.256e9345.js";import"./index.37ff117c.js";import"./index.683f5c3f.js";import"./index.1ec10480.js";import"./index.9aac1c9e.js";import"./index.d8cce3db.js";import"./index.24cd6faf.js";import"./iframe.d7e5954c.js";const w={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
+
+import ContactTemplate, { ContactTemplateProps } from '.';
+
+export default {
+  title: 'Templates / ContactTemplate',
+  component: ContactTemplate,
+} as ComponentMeta<typeof ContactTemplate>;
+
+export const API = (props: ContactTemplateProps) => {
+  return (
+    <div tw="bg-primary">
+      <ContactTemplate {...props} />
+    </div>
+  );
+};
+`,locationsMap:{api:{startLoc:{col:19,line:11},endLoc:{col:1,line:17},startBody:{col:19,line:11},endBody:{col:1,line:17}}}}},title:"Templates / ContactTemplate",component:r},P=o(t=>e(m,{children:e(r,{...t})}),"API");var m=a("div").withConfig({displayName:"indexstories___StyledDiv",componentId:"sc-mqkuwv-0"})({"--tw-bg-opacity":"1",backgroundColor:"rgb(var(--color-primary) / var(--tw-bg-opacity))"});const I=["API"];export{P as API,I as __namedExportsOrder,w as default};
+//# sourceMappingURL=index.stories.2f0427d6.js.map
