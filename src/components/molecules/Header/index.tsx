c4ae4c95d1c977a47c88c9e85061b8442ae1c5e7
seraheo <heosera76@gmail.com>
2022-12-09 17:29:11 +0900

    Header 컴포넌트 반응형 구현 및 문서화

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -5,24 +5,26 @@ import Link from '../../atoms/Link';
 
 const Header = () => {
   return (
-    <div tw="w-full bg-primary px-28 py-10 grid grid-cols-3">
+    <div tw="w-full bg-primary lg:px-28 md:px-16 px-4 md:py-10 py-4 grid grid-cols-3">
       <div tw="flex justify-start ">
         <Link
           href="https://quessr.tistory.com/"
           size="md"
-          tw="text-secondary flex items-center "
+          tw="text-secondary flex items-center scale-75 md:scale-100"
         >
           Blog
         </Link>
-        {/* <Button>left2</Button> */}
       </div>
       <div tw="flex justify-center">
-        <Link href="/" size="lg" tw="text-secondary">
+        <Link href="/" size="lg" tw="text-secondary scale-75 md:scale-100">
           Portfolio
         </Link>
       </div>
-      <div tw="flex justify-end">
-        <Button size="md" variant="outline">
+      <div tw="flex justify-end items-center">
+        <Button size="md" variant="outline" tw="hidden sm:flex">
+          Contact Me
+        </Button>
+        <Button size="xs" variant="outline" tw="sm:hidden">
           Contact Me
         </Button>
       </div>
