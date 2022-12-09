65b7d3ba1b6f77f08bf8a62ae39e7a640ba9a643
seraheo <heosera76@gmail.com>
2022-12-09 18:08:11 +0900

    ContactTemplate 컴포넌트 반응형 구현 및 문서화

--- src/components/templates/ContactTemplate/index.tsx
+++ src/components/templates/ContactTemplate/index.tsx
@@ -10,15 +10,15 @@ export type ContactTemplateProps = ContactFormProps;
 
 const ContactTemplate = ({ onSubmit }: ContactTemplateProps) => {
   return (
-    <div tw="grid gap-y-28 bg-primary">
+    <div tw="grid lg:gap-y-24 md:gap-y-14 gap-y-8 bg-primary">
       <Header />
 
-      <div tw="grid grid-cols-2 px-28">
+      <div tw="grid md:grid-cols-2 sm:grid-cols-1 lg:px-28 md:px-16 px-8 gap-y-8">
         {/* Get in Touch */}
         <div tw="text-secondary h-[280px] grid">
-          <h3 tw="text-4xl">Get in Touch</h3>
-          <p tw="text-xl">heosera76@gmail.com</p>
-          <p tw="text-xl">010 - 7626 - 7410</p>
+          <h3 tw="md:text-4xl text-3xl">Get in Touch</h3>
+          <p tw="md:text-xl text-lg">Quessr00@gmail.com</p>
+          <p tw="md:text-xl text-lg">010 - 7626 - 7410</p>
           <div tw="flex fill-secondary gap-3">
             <TwitterIcon />
             <GithubIcon />
