8f81eea2c46553b6cb7795c54323655aaa8c6b88
Quessr <Quessr@users.noreply.github.com>
2022-12-13 14:37:52 +0000

    deploy: e5069491ea65b0da6f2763bc3064a3bd917cb829

---
+++ assets/index.stories.8551e80a.js
@@ -0,0 +1,19 @@
+var p=Object.defineProperty;var o=(t,n)=>p(t,"name",{value:n,configurable:!0});import{H as a}from"./styled-components.browser.esm.2dc388f3.js";import{C as e}from"./index.e4a1ac0d.js";import{j as r}from"./jsx-runtime.7979c991.js";import"./index.d4a592cd.js";import"./index.b521ceb1.js";import"./index.9b64af41.js";import"./clsx.m.256e9345.js";import"./index.6ee2f45f.js";import"./index.614d0ffb.js";import"./index.67c23d74.js";import"./index.e8bf1f26.js";import"./index.0aee9c7c.js";import"./index.6e59ab12.js";import"./index.67f53b60.js";import"./index.3257d584.js";import"./index.3d1c05c6.js";import"./iframe.2ca452d4.js";const k={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
+
+import ContactTemplate, { ContactTemplateProps } from '.';
+
+export default {
+  title: 'Templates / ContactTemplate',
+  component: ContactTemplate,
+} as ComponentMeta<typeof ContactTemplate>;
+
+export const API = (props: ContactTemplateProps) => {
+  return (
+    <div tw="bg-primary">
+      <ContactTemplate {...props} />
+    </div>
+  );
+};
+`,locationsMap:{api:{startLoc:{col:19,line:11},endLoc:{col:1,line:17},startBody:{col:19,line:11},endBody:{col:1,line:17}}}}},title:"Templates / ContactTemplate",component:e},A=o(t=>r(m,{children:r(e,{...t})}),"API");var m=a("div").withConfig({displayName:"indexstories___StyledDiv",componentId:"sc-mqkuwv-0"})({"--tw-bg-opacity":"1",backgroundColor:"rgb(var(--color-primary) / var(--tw-bg-opacity))"});const M=["API"];export{A as API,M as __namedExportsOrder,k as default};
+//# sourceMappingURL=index.stories.8551e80a.js.map
---
+++ assets/index.stories.bae12c6c.js
@@ -0,0 +1,15 @@
+var n=Object.defineProperty;var e=(o,t)=>n(o,"name",{value:t,configurable:!0});import{B as r}from"./index.6e59ab12.js";import{j as p}from"./jsx-runtime.7979c991.js";import"./styled-components.browser.esm.2dc388f3.js";import"./index.67f53b60.js";import"./index.9b64af41.js";import"./clsx.m.256e9345.js";import"./index.3257d584.js";import"./iframe.2ca452d4.js";const g={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
+
+import BlogHeader from '.';
+
+export default {
+  title: 'Molecules / BlogHeader',
+  component: BlogHeader,
+} as ComponentMeta<typeof BlogHeader>;
+
+export const API = (props: typeof BlogHeader) => {
+  return <BlogHeader {...props} />;
+};
+`,locationsMap:{api:{startLoc:{col:19,line:11},endLoc:{col:1,line:13},startBody:{col:19,line:11},endBody:{col:1,line:13}}}}},title:"Molecules / BlogHeader",component:r},u=e(o=>p(r,{...o}),"API"),H=["API"];export{u as API,H as __namedExportsOrder,g as default};
+//# sourceMappingURL=index.stories.bae12c6c.js.map
