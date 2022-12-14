be22bbf6fba9293692baaad1bbf64a90e98592c8
Quessr <Quessr@users.noreply.github.com>
2022-12-14 15:48:48 +0000

    deploy: 272ec6181d0b8ee7fef4de3ac7f1919137c8f956

---
+++ assets/index.stories.023c2a84.js
@@ -0,0 +1,19 @@
+var n=Object.defineProperty;var e=(o,r)=>n(o,"name",{value:r,configurable:!0});import{H as t}from"./index.860457d8.js";import{j as a}from"./jsx-runtime.36e1dd74.js";import"./styled-components.browser.esm.2268c1cf.js";import"./index.f77a44b8.js";import"./index.e7a94e70.js";import"./clsx.m.256e9345.js";import"./index.a847c39a.js";import"./iframe.79673b98.js";const H={parameters:{storySource:{source:`import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
+
+import Header from '.';
+
+export default {
+  /* \u{1F447} The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Molecules / Header',
+  component: Header,
+} as ComponentMeta<typeof Header>;
+
+export const API = (props: typeof Header) => {
+  return <Header {...props} />;
+};
+`,locationsMap:{api:{startLoc:{col:19,line:15},endLoc:{col:1,line:17},startBody:{col:19,line:15},endBody:{col:1,line:17}}}}},title:"Molecules / Header",component:t},y=e(o=>a(t,{...o}),"API"),x=["API"];export{y as API,x as __namedExportsOrder,H as default};
+//# sourceMappingURL=index.stories.023c2a84.js.map
